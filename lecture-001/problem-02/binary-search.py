97b95398c9586bf707d25bab48e36546fb1e3909
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2022-10-16 23:01:50 +0530

    feat: :sparkles: add solution for binary search

---
+++ lecture-001/problem-02/binary-search.py
@@ -0,0 +1,19 @@
+def binary_search(nums: list[int], key: int) -> int:
+    lb = 0
+    ub = len(nums) - 1
+
+    while lb <= ub:
+        mid = lb + ((ub - lb) // 2)
+
+        if nums[mid] == key: return mid
+        elif nums[mid] > key: ub = mid - 1
+        else: lb = mid + 1
+
+    return -1
+
+
+print(binary_search([10, 20, 30, 40, 50, 60, 70], 70))
+print(binary_search([10, 20, 30, 40, 50, 60, 70], 20))
+print(binary_search([10, 20, 30, 40, 50, 60, 70], 10))
+print(binary_search([10, 20, 30, 40, 50, 60, 70], 100))
+print(binary_search([10, 20, 30, 40, 50, 60, 70], 0))
