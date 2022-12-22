7a8c35c9ee8c9e99136e8d7062fcd33ac649c9d0
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2022-12-22 21:23:21 +0530

    feat: :sparkles: add solution for unordered priority queue

---
+++ lecture-003/problem-03/unordered-priority-queue.py
@@ -0,0 +1,60 @@
+class UnorderedPriorityQueue:
+    def __init__(self) -> None:
+        self.queue: list[int] = []
+        
+    def enqueue(self, num: int) -> None:
+        self.queue.append(num)
+        
+    def dequeue(self) -> int:
+        if len(self.queue) == 0: raise Exception("Queue Underflow Exception!!")
+        if len(self.queue) == 0: return self.queue.pop()
+        
+        lowestIndex = 0
+        
+        for i in range(1, len(self.queue)):
+            if self.queue[lowestIndex] > self.queue[i]: lowestIndex = i
+            
+        poppedValue = self.queue[lowestIndex]
+            
+        for i in range(lowestIndex, len(self.queue) - 1):
+            self.queue[i] = self.queue[i+1]
+            
+        self.queue.pop()
+        
+        return poppedValue
+    
+    def isEmpty(self) -> bool: return len(self.queue) == 0
+    
+    def top(self) -> int:
+        if len(self.queue) == 0: raise Exception("Queue Underflow Exception!!")
+        
+        lowestIndex = 0
+        
+        for i in range(1, len(self.queue)):
+            if self.queue[lowestIndex] > self.queue[i]: lowestIndex = i
+            
+        return self.queue[lowestIndex]
+    
+    def display(self) -> None:
+        for num in self.queue:
+            print(num)
+            
+            
+priority_queue = UnorderedPriorityQueue()
+
+priority_queue.enqueue(45)
+priority_queue.enqueue(15)
+priority_queue.enqueue(16)
+priority_queue.enqueue(25)
+priority_queue.enqueue(18)
+priority_queue.enqueue(20)
+priority_queue.enqueue(77)
+
+print(priority_queue.top())
+
+print(priority_queue.isEmpty())
+
+print(priority_queue.dequeue())
+
+print("The priority queue is: ")
+priority_queue.display()
