e69043c56f980090905a9358fd2e103d7a2276c9
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-01-05 12:18:52 +0530

    feat: :sparkles: add graph dft

---
+++ graph/dfs/dfs.py
@@ -0,0 +1,64 @@
+"""
+    ? Problem Statement: DFT Graph
+    
+    ! Time Complexity: O(v + e)
+    ! Space Complexity: O(v)
+"""
+
+def dfs(edges: list[list[int]], graphType: str, startNode: int or None = None) -> None:
+    """
+        graphType refers to undirected or directed type passed as string. It is also a mandatory parameter.
+    """
+    if len(edges) == 0: return
+    
+    adjacencyList = getAdjacencyList(edges, graphType)
+    
+    dfsStack: list[int] = []
+    visitedSet: set[int] = set()
+    
+    if startNode is None:
+        dfsStack.append(edges[0][0])
+        visitedSet.add(edges[0][0])
+    else:
+        dfsStack.append(startNode)
+        visitedSet.add(startNode)
+    
+    
+    while len(dfsStack) > 0:
+        currentNode = dfsStack.pop()
+        adjacentNodes = adjacencyList[currentNode]
+        
+        for adjacentNode in adjacentNodes:
+            if adjacentNode not in visitedSet:
+                dfsStack.append(adjacentNode)
+                visitedSet.add(adjacentNode)
+                
+        print(currentNode)
+    
+    
+
+def getAdjacencyList(edges: list[list[int]], graphType) -> dict[int, list[int]]:
+    adjacencyList: dict[int, list[int]] = dict()
+    
+    for edge in edges:
+        if edge[0] not in adjacencyList:
+            adjacencyList[edge[0]] = [edge[1]]
+        else:
+            adjacencyList[edge[0]].append(edge[1])
+        
+        if graphType == "undirected":   
+            if edge[1] not in adjacencyList:
+                adjacencyList[edge[1]] = [edge[0]]
+            else:
+                adjacencyList[edge[1]].append(edge[0])
+            
+    return adjacencyList
+
+dfs([
+    [0, 1],
+    [0, 2],
+    [1, 2],
+    [2, 0],
+    [2, 3],
+    [3, 3]
+], "directed", 1)
