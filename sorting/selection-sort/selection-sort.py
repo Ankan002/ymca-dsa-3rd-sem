6ac4e83c327fe53651236a9aa5baaa934c185a04
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-01-12 23:50:08 +0530

    feat: :sparkles: update solution for selection sort

--- sorting/selection-sort/selection-sort.py
+++ sorting/selection-sort/selection-sort.py
@@ -1,15 +1,13 @@
 def selectionSort(arr: list[int]) -> None:
     for i in range(len(arr) - 1):
-        pivot = arr[i]
         smallest_index = i
         
         for j in range(i, len(arr)):
-            if arr[j] < pivot:
+            if arr[j] < arr[smallest_index]:
                 smallest_index = j
                 
-        print(arr[smallest_index])
-        print(arr)
-        [arr[i], arr[smallest_index]] = [arr[smallest_index], arr[i]]
+        if i != smallest_index:
+            [arr[i], arr[smallest_index]] = [arr[smallest_index], arr[i]]
             
             
 nums = [4, 3, 11, 23, 17, 13, 199, 121, 147, 179]
